81b5f5627570f61fc431ad7b3866ebb5cfa3fc7f
Thien Long <longnguyyen97@gmail.com>
2020-06-25 15:19:08 +0700

    working with object property

--- first-app.js
+++ first-app.js
@@ -1,25 +1,8 @@
-const name = 'David'; //const don't change the value of its variable
-let age = 22; //let can LET you change the value
-const hasHobbies = true;
-
-age = 30;
-
-const summmarizeUser = (userName, userAge, userHasHobbies) => {
-    return (
-        'Name is ' + 
-         userName + 
-        ', age is ' + 
-         userAge + 
-        ' and the user has hobbies: ' 
-        + userHasHobbies
-        );
-}
-
-// const add = (a,b) => {
-//     return a+b;
-// }
-//const addOne = a => a + 1;
-const addRandom = () => 1+2;
-
-console.log(addRandom());
-console.log(summmarizeUser(name, age, hasHobbies));
+const person = {
+    name: 'David',
+    age: 22,
+    greet() {
+        console.log('Hi, my name is ' + this.name);
+    }
+};
+person.greet();
