f7372aadbb3d91052527824b02c108a0cac9e5de
Thien Long <longnguyyen97@gmail.com>
2020-06-25 16:02:53 +0700

    understanding destruturing

--- first-app.js
+++ first-app.js
@@ -12,23 +12,36 @@ const person = {
     }
 };
 
-const copiedPersion = {...person};
-console.log(person);
+const printName = ({ name }) => 
+{
+    console.log(name);
+}
+
+printName(person);
+
+const {name, age} = person;
+console.log(name,age); 
+
+// const copiedPersion = {...person};
+// console.log(person);
 
 const hobbies = ['Sports', 'Reading'];
-// for(let hobby of hobbies) // "let" here will store every element of hobbies array to "hobby"
-// {
-//     console.log(hobby);
-// }
-// console.log(hobbies.map(hobby =>  'Hobby: ' + hobby )); //map give you a new array to edit it whatever you one and 
-// console.log(hobbies);
-//const copiedArray = hobbies.slice(); // slice coppies an array, we can pass arguments to narrow down the range of elements we want
-const copiedArray = [...hobbies]; //created new array where the first element is the OLD array
-// "..." take the array or objects after the operator and pull out all the elements or properties and put it whatever around that spread operator
-console.log(copiedArray);
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1,hobby2); //this right here log 2 individual values from hobbies array via array destruturing
 
-const toArray = (...args) => // rest operators here will take all arguments we specified not matter how many of it and bundle it to an array
-{
-    return args;
-};
-console.log(toArray(1,2,3,4,5,6));
+// // for(let hobby of hobbies) // "let" here will store every element of hobbies array to "hobby"
+// // {
+// //     console.log(hobby);
+// // }
+// // console.log(hobbies.map(hobby =>  'Hobby: ' + hobby )); //map give you a new array to edit it whatever you one and 
+// // console.log(hobbies);
+// //const copiedArray = hobbies.slice(); // slice coppies an array, we can pass arguments to narrow down the range of elements we want
+// const copiedArray = [...hobbies]; //created new array where the first element is the OLD array
+// // "..." take the array or objects after the operator and pull out all the elements or properties and put it whatever around that spread operator
+// console.log(copiedArray);
+
+// const toArray = (...args) => // rest operators here will take all arguments we specified not matter how many of it and bundle it to an array
+// {
+//     return args;
+// };
+// console.log(toArray(1,2,3,4,5,6));
