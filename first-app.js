62f69224e8dfa4c75060f1378e567f48049a4e60
Thien Long <longnguyyen97@gmail.com>
2020-06-25 15:13:33 +0700

    understanding arrow function

--- first-app.js
+++ first-app.js
@@ -2,10 +2,11 @@ const name = 'David'; //const don't change the value of its variable
 let age = 22; //let can LET you change the value
 const hasHobbies = true;
 
-function summmarizeUser(userName, userAge, userHasHobbies)
-{
+age = 30;
+
+const summmarizeUser = (userName, userAge, userHasHobbies) => {
     return (
-        'Name is' + 
+        'Name is ' + 
          userName + 
         ', age is ' + 
          userAge + 
@@ -14,4 +15,11 @@ function summmarizeUser(userName, userAge, userHasHobbies)
         );
 }
 
+// const add = (a,b) => {
+//     return a+b;
+// }
+//const addOne = a => a + 1;
+const addRandom = () => 1+2;
+
+console.log(addRandom());
 console.log(summmarizeUser(name, age, hasHobbies));
