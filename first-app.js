aaddca6180293a56bbc5dbeffe9135a3d53eb5c6
Thien Long <longnguyyen97@gmail.com>
2020-06-25 15:04:41 +0700

    differences between let and const variable

--- first-app.js
+++ first-app.js
@@ -1,3 +1,17 @@
-const fs = require('fs');
+const name = 'David'; //const don't change the value of its variable
+let age = 22; //let can LET you change the value
+const hasHobbies = true;
 
-fs.writeFileSync('hello.txt', 'hello from node.js');
+function summmarizeUser(userName, userAge, userHasHobbies)
+{
+    return (
+        'Name is' + 
+         userName + 
+        ', age is ' + 
+         userAge + 
+        ' and the user has hobbies: ' 
+        + userHasHobbies
+        );
+}
+
+console.log(summmarizeUser(name, age, hasHobbies));
